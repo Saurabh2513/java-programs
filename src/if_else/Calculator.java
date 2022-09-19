a226ba0dd7cbe3532dea4c12250b2871c80e8a9e
Sagar Yadav <sagaryadav12369@gmail.com>
2022-09-19 22:50:26 +0530

    new added program

--- src/if_else/Calculator.java
+++ src/if_else/Calculator.java
@@ -5,20 +5,30 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        int num;
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("enter a number: ");
-        num= scanner.nextInt();
-        System.out.println("Please select operation:\n" +
-                "1.Addition" +
-                "2.Subtraction" +
-                "3.Division" +
-                " 4.Multiplication");
+        int num1, num2;
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter num1 number: ");
+        num1 = scanner.nextInt();
+        System.out.println("enter num2 number: ");
+        num2 = scanner.nextInt();
+        System.out.println("Please select operation :\n"
+                + "1.Addition: \n" + "2.Subtraction: \n" + "3.Subtraction \n" + "4.Multiplication \n");
+        Scanner op = new Scanner(System.in);
+        String operation = op.next();
 
+        if (operation.equals("+")) {
+            System.out.println("Your Answer: " + (num1 + num2));
+            if (operation.equals("-")) {
+                System.out.println("Your Answer: " + (num1 - num2));
+                if (operation.equals("%")) {
+                    System.out.println("Your Answer: " + (num1 % num2));
+                    if (operation.equals("*")) {
+                        System.out.println("Your Answer: " + (num1 * num2));
+                    }
+                }
 
-
-
-        System.out.println("");
+            }
+        }
     }
 }
