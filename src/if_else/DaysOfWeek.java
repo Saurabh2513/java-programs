846844f47b225bf76495e59ef34c1a557edd7eb2
Sagar Yadav <sagaryadav12369@gmail.com>
2022-09-20 09:52:36 +0530

    Days of week program

---
+++ src/if_else/DaysOfWeek.java
@@ -0,0 +1,38 @@
+package if_else;
+import java.util.Scanner;
+
+//8. Print days of week (use if-else or switch)
+
+public class DaysOfWeek {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter a week number: ");
+        int week= scanner.nextInt();
+        switch (week){
+                case 1:
+                System.out.println("sunday");
+                break;
+                case 2:
+                System.out.println("monday");
+                break;
+                case 3:
+                System.out.println("tuesday");
+                break;
+                case 4:
+                System.out.println("wednesday");
+                break;
+                case 5:
+                System.out.println("thursday");
+                break;
+                case 6:
+                System.out.println("friday");
+                break;
+                case 7:
+                System.out.println("saturday");
+                break;
+                default:
+                System.out.println("this number is invalid");
+                break;
+        }
+    }
+}
