850b5b98d3a2627e01063298cfebf84e5e4c5a78
Sagar Yadav <sagaryadav12369@gmail.com>
2022-09-20 16:15:24 +0530

    Student Marks program

---
+++ src/if_else/StudentMarks.java
@@ -0,0 +1,30 @@
+package if_else;
+import java.util.Scanner;
+/*10. Create program to determine Student is Pass or Fail.
+        Take marks of 5 subjects.
+        English, Marathi, Hindi, Maths, Geo.
+        Check if Student has failed in any subject. if student is failed in any subject then show the subject name and marks.
+        If student is pass then show the percentage.
+ */
+public class StudentMarks {
+  public static void main(String[] args) {
+    int count = 0;
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("enter student name:");
+    String name = scanner.next();
+    System.out.println("enter student marks:");
+    int English, Marathi, Hindi, Maths, Geo;
+    System.out.println("English: ");
+    int english = scanner.nextInt();
+    System.out.println("Marathi: ");
+    int marathi = scanner.nextInt();
+    System.out.println("Hindi: ");
+    int hindi = scanner.nextInt();
+    System.out.println("Maths: ");
+    int maths = scanner.nextInt();
+    System.out.println("Geo: ");
+    int geo = scanner.nextInt();
+
+    }
+  }
+
