738abb9c8698458f2027d9d0bb0b4b022b06f2c6
Sagar Yadav <sagaryadav12369@gmail.com>
2022-09-20 22:18:08 +0530

    Marks program

--- src/if_else/StudentMarks.java
+++ src/if_else/StudentMarks.java
@@ -8,23 +8,64 @@ import java.util.Scanner;
  */
 public class StudentMarks {
   public static void main(String[] args) {
-    int count = 0;
+    int count = 0,total=0;
+    char operator = 0,percentage=0;
     Scanner scanner = new Scanner(System.in);
     System.out.println("enter student name:");
     String name = scanner.next();
-    System.out.println("enter student marks:");
-    int English, Marathi, Hindi, Maths, Geo;
-    System.out.println("English: ");
+
+    System.out.println("enter student marks: \nEnglish: ");
     int english = scanner.nextInt();
+
     System.out.println("Marathi: ");
     int marathi = scanner.nextInt();
+
     System.out.println("Hindi: ");
     int hindi = scanner.nextInt();
+
     System.out.println("Maths: ");
     int maths = scanner.nextInt();
+
     System.out.println("Geo: ");
     int geo = scanner.nextInt();
 
+    if(100>=30){
+      System.out.println("Student is fail in English he has 30 marks.");
+    }
+    else if(100>=30){
+      System.out.println("Student is fail in Marathi he has 30 marks.");
+    }
+    else if(100>=30){
+      System.out.println("Student is fail in Hindi he has 30 marks.");
+    }
+    else if(100>=30){
+      System.out.println("Student is fail in Maths he has 30 marks.");
+    }
+    else if(100>=30){
+      System.out.println("Student is fail in Geo he has 30 marks.");
+    }
+    else{
+      System.out.println("student has all subject clear");
+    }
+
+    System.out.println("total of all marks please enter '+'");
+    operator = scanner.next().charAt(0);
+
+    switch (operator){
+      case '+':
+        total = english + marathi + hindi + maths + geo;
+        System.out.println("Total is: " + total);
+
+        System.out.println("percentage of all marks please enter '%'");
+        operator = scanner.next().charAt(0);
+
+      case '%':
+        percentage = (char) ((total / 500)*100);
+        System.out.println("Total Percentage : " + percentage + "%");
+
+        }
     }
   }
+
+
 
