78064907b882e39b8ed7487f6ebf5ca5db82ed49
Sagar Yadav <sagaryadav12369@gmail.com>
2022-09-20 23:09:15 +0530

    loop 1,2 program

---
+++ src/loop/EvenNumber.java
@@ -0,0 +1,11 @@
+package loop;
+//2. Print Even Numbers between 1 - 100
+public class EvenNumber {
+
+    public static void main(String[] args) {
+        for(int i = 0; i <= 50; i++)
+        {
+            System.out.println(i*2);
+        }
+    }
+}
---
+++ src/loop/PrintNumber.java
@@ -0,0 +1,10 @@
+package loop;
+//1. Print numbers between 1 - 100
+public class PrintNumber {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 100; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
