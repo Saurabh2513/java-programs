2ab24828e35e576b8739a0d9b4f7c8923fa729fe
Sagar Yadav <sagaryadav12369@gmail.com>
2022-09-20 23:17:05 +0530

    loop 1,2,3 program

--- src/loop/OddNumber.java
+++ src/loop/OddNumber.java
@@ -1,10 +1,9 @@
 package loop;
-//2. Print Even Numbers between 1 - 100
-public class EvenNumber {
-
+//3. Print Odd Numbers between 1 - 100
+public class OddNumber {
     public static void main(String[] args) {
         for (int i = 0; i <= 100; i++) {
-            if (i % 2 == 0) {
+            if (i % 2 == 1) {
                 System.out.print(i + "\n");
             }
         }
